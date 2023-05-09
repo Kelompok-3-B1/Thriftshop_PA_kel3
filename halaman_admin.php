0bca29eae4d9bd7072612d3710ff6ed6b9a3074e
Chorine88 <chorine.jessica@gmail.com>
2023-05-09 21:57:15 +0800

    menambahkan file halaman admin

---
+++ halaman_admin.php
@@ -0,0 +1,52 @@
+<?php 
+    session_start();
+
+    // cek apakah yang mengakses halaman ini sudah login
+    if($_SESSION['level']==""){
+        header("location:login.php?pesan=gagal");
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thrift Shop</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">    
+    <link href="https://fonts.googleapis.com/css2?family=Oxygen&display=swap" rel="stylesheet">
+</head>
+<body>
+
+    <!-- header -->
+    <header>
+        <div class="container">
+            <h1><a href="halaman_admin.php">THRIFT SHOP</a></h1>
+            <ul>
+                <li><a href="halaman_admin.php">Dashboard</a></li>
+                <li><a href="profil.php">Profil</a></li>
+                <li><a href="data-kategori.php">Data Kategori</a></li>
+                <li><a href="data-produk.php">Data Produk</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        </div>
+    </header> 
+
+    <!-- content -->
+    <div class="section">
+        <div class="container">
+            <h3>Dashboard</h3>
+            <div class="box">
+                <h4>Halo <b><?php echo $_SESSION['user']; ?></b> Anda telah login sebagai <b><?php echo $_SESSION['level']; ?></b>.</h4>
+            </div>
+        </div>  
+    </div>
+
+    <!-- footer -->
+    <footer>
+        <div class="container">
+            <small>Copyright &copy; 2023 - THRIFT SHOP.</small>
+        </div>
+    </footer>
+</body>
+</html>
